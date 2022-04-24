66b63d6e1506ed917e3542194c6df1bc52cd81f1
DecafSunrise <36832027+DecafSunrise@users.noreply.github.com>
2022-04-24 02:38:33 -0400

    Add error handling

--- Read_RSS.py
+++ Read_RSS.py
@@ -17,15 +17,22 @@ import uuid
 from urllib.parse import urlparse
 
 name = 'Operation_Lonestar'
+con = sqlite3.connect(f"{name}_db.sqlite")
+
 url = 'https://www.google.com/alerts/feeds/14966249695842301360/8191453454664689556'
 resp = requests.get(url)
 soup = BeautifulSoup(resp.text,'html.parser')
+
+try:
+    df_existing = pd.read_sql_query("SELECT * from RSS_Hits", con)
+except:
+    df_existing = pd.DataFrame(columns=['GUID', 'Title', 'Date_Published', 'Date_Retrieved', 'Excerpt',
+       'Google_Link', 'Link', 'Site'])
 
 output = []
 for entry in soup.find_all('entry'):
 
     item = {
-        'GUID': str(uuid.uuid4()),
         'Title' : entry.find('title',{'type':'html'}).text,
         'Date_Published' : entry.find('published').text,
         'Date_Retrieved': str(datetime.utcnow()),
@@ -36,6 +43,7 @@ for entry in soup.find_all('entry'):
     output.append(item)
 
 df = pd.DataFrame(output)
+df['GUID'] = [str(uuid.uuid4()) for x in range(len(df))]
 
 def extractUrl(GALink):
     match = re.findall(r"(?:&url=)(.*)(?:&ct=)", GALink)
@@ -49,11 +57,15 @@ def getBaseURL(link):
 
 df['Link'] = df['Google_Link'].apply(extractUrl)
 df['Site'] = df['Link'].apply(getBaseURL)
+# df['Hash'] = df[['Title', 'Excerpt', 'Google_Link', 'Link']].apply(lambda x: hash(tuple(x)), axis = 1)
 
-con = sqlite3.connect(f"{name}_db.sqlite")
+
+# Compare the links, only write the new stuff
+old_links= df_existing['Link'].tolist()
+df_out = df[~df['Link'].isin(old_links)]
 
 # Write the new DataFrame to a new SQLite table
-df.to_sql("RSS_Hits", con, if_exists="append", index=False)
+df_out.to_sql("RSS_Hits", con, if_exists="append", index=False)
 
 con.close()
 
@@ -61,6 +73,9 @@ now = datetime.utcnow()
 with open(fr"./Logs/Read_RSS_run_{str(now.strftime('%m_%d_%Y, %H_%M_%S'))}.txt", 'w') as f:
     f.write(str(now)+"\n")
     f.write(f"Scraped {len(df)} links")
+
+print(f"Read {len(df)} links")
+print(f"Scraped {len(df_out)} new links")
 
 print("Done!")
 
